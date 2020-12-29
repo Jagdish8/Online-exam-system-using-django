d7cbcd4c7ece355bf6b453baabc859e5194aa7f9
Jagdish8 <76528713+Jagdish8@users.noreply.github.com>
2020-12-30 00:13:33 +0530

    Update settings.py

--- onlineexam/settings.py
+++ onlineexam/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','pure-mountain-07734.herokuapp.com']
+ALLOWED_HOSTS = ['pure-mountain-07734.herokuapp.com','127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
